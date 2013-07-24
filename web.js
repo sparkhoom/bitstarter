22173598767dfc27c57f2b25f4c5682db79a33fd
Sparkhoom <kickyourass09@gmail.com>
2013-07-24 01:10:04 +0000

    readfile

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!2');
+  fs.readfile('index.html', function (err, data){
+    if (err) throw err;
+    response.send('Hello World!2');
+  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+  
